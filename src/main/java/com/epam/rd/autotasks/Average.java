f29ec77e3b1fcec760436a2ad056256d983206e0
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-07 23:14:10 +0300

    Average

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
-
+        int sum=0,number,size=-1;
+        do {
+            number=scanner.nextInt();
+            sum+=number;
+            size++;
+        }
+        while (number!=0);
+        System.out.println(sum/size);
     }
 
 }
